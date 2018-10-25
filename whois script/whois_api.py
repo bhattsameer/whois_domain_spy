248d5b2ee884fe4f559db6a805efda44e56522d7
WJ Welch <wwelch@ctcd.edu>
2018-10-24 21:57:33 -0500

    Adjusted import libraries. Output file not fully correct.

--- whois script/whois_api.py
+++ whois script/whois_api.py
@@ -1,13 +1,16 @@
-import urllib2
+# Asjustments to repo to migrate from Python 2 to Python 3
+# Primary documentation is in README.md. Additional, specific program notes are here.
+
+import urllib.request
 import json
 
 
 Api_url = "http://whoiz.herokuapp.com/lookup.json?url="
-url = raw_input("Enter website example: google.com  => ")
+url = input("Enter website example: google.com  => ")
 
 f = open("whois_output.txt","w")
 
-json_data = urllib2.urlopen(Api_url+url)
+json_data = urllib.request.urlopen(Api_url+url)
 
 data = json.load(json_data)
 f.write(data['disclaimer']+"\n\n")
